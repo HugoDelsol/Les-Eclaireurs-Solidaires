c8f7b8d279e376ef1dc213c84f715bef259b2a3d
Hugo Dels <hugo.delsol64@gmail.com>
2026-03-05 14:43:01 +0100

    freeez + css

--- site/src/middleware/sideNav.middleware.js
+++ site/src/middleware/sideNav.middleware.js
@@ -1,7 +1,10 @@
 const tabSelected = (req, res, next) => {
-    const url = req.url
-    res.locals.urlFind = url
-    next()
+    
+    const url = req.url;
+    const myReg = /[\d]/g;
+    const urlReg = url.replace(myReg, "");
+    res.locals.urlFind = urlReg;
+    next();
 }
 
 module.exports = {
