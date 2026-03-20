00dba703302e74a964de1b4ea75453eafc77f840
Hugo Dels <hugo.delsol64@gmail.com>
2026-03-20 11:00:11 +0100

    save-20/03/2026

--- site/src/public/javaScript/loginDisplay.js
+++ site/src/public/javaScript/loginDisplay.js
@@ -6,15 +6,17 @@ const imgSrc = document.querySelector('.imgSrc');
 const inputPswd = document.querySelector('.password');
 
 imgSrc.addEventListener('click', () => {
-
+    
     if (imgSrc.attributes.src.nodeValue === "/pictures/globalIcons/closeEye.png") {
-
+        
         imgSrc.attributes.src.nodeValue = "/pictures/globalIcons/openEye.png";
+        imgSrc.attributes.alt.nodeValue = "Masquer le mot de passe";
         inputPswd.type = "text";
-
+        
     } else if (imgSrc.attributes.src.nodeValue === "/pictures/globalIcons/openEye.png") {
-
+        
         imgSrc.attributes.src.nodeValue = "/pictures/globalIcons/closeEye.png";
+        imgSrc.attributes.alt.nodeValue = "Afficher le mot de passe";
         inputPswd.type = "password";
     }
-})
+});
