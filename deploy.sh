f5c8f237098d4a0ce073aab308390148e9ce2136
Hugo Delsol <hugo.delsol64@gmail.com>
2026-06-19 09:58:35 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -234,7 +234,7 @@ NGINX_CONF="/etc/nginx/sites-available/$APP_NAME"
 sudo bash -c "cat > $NGINX_CONF" <<EOF
 server {
         listen 80;
-        server_name 141.227.150.65;
+        server_name .........;
 
         # Optimisation Éco-conception : Compression Gzip active
         gzip on;
